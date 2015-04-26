660f5038003fb0de341eb6d7cddc8a86d8a7ef0b
cocawang <coca_wang@hotmail.com>
2015-04-25 22:27:48 -0400

    data Signed-off-by: cocawang <coca_wang@hotmail.com>

---
+++ run_analysis.R
@@ -0,0 +1,35 @@
+x_train <- read.table("d:/Project/X_train.txt")
+x_test <- read.table("d:/Project/X_test.txt")
+y_train <- read.table("d:/Project/y_train.txt")
+y_test <- read.table("d:/Project/y_test.txt")
+subject_train <- read.table("d:/Project/subject_train.txt")
+subject_test <- read.table("d:/Project/subject_test.txt")
+train <- data.frame(x_train,y_train,subject_train)
+test <- data.frame(x_test,y_test,subject_test)
+entire_data <- rbind(train, test)    ##merge datasets
+
+feature <- read.table("d:/Project/features.txt")
+feature1 <- feature[grep("std()", feature$V2), ]
+feature2 <- feature[grep("mean()", feature$V2,fixed=TRUE), ]
+feature <- rbind(feature1,feature2)
+feature <- feature[order(feature$V1),]
+
+measures <-entire_data[,c(feature$V1,562,563)] ## Extract Mean and STD variables
+
+## name activity
+measures$V1.1[measures$V1.1 == "1"] <- "walking"
+measures$V1.1[measures$V1.1 == "2"] <- "walking_upstairs"
+measures$V1.1[measures$V1.1 == "3"] <- "walking_downstairs"
+measures$V1.1[measures$V1.1 == "4"] <- "sitting"
+measures$V1.1[measures$V1.1 == "5"] <- "standing"
+measures$V1.1[measures$V1.1 == "6"] <- "laying"
+
+names(measures)[1:66] <- as.character(feature$V2)
+names(measures)[67] <- "activity"
+names(measures)[68] <- "subject"
+
+measures$subject <- as.character(measures$subject)
+measures1 <- measures[,1:66]
+cleandata <- aggregate(measures1, by=list(measures$activity,measures$subject), mean)
+
+write.table(cleandata, file="clean_data.txt",row.names=FALSE)
